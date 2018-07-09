847b548e52feedfaf9a8069d998ec95144412d9b
Julia Tan <emodrumsophatic@gmail.com>
2018-07-09 15:57:31 +0200

    More colours and shapes

--- sketch.js
+++ sketch.js
@@ -1,17 +1,29 @@
 function setup() {
   // put setup code here
   createCanvas(600,400);
+    background('black');
 }
+
 
 function draw() {
   // put drawing code here
-  background('green');
+
 
 //this circle will follow my mouse
-fill("gray");
-ellipse(mouseX,mouseY,40,40);
+fill("pink");
+ellipse(mouseX,mouseY,5,5);
+ noStroke();
+
+ fill("green");
+ ellipse(mouseX-6,mouseY+20,2,2);
+  noStroke();
+
+fill("white");
+ triangle(30, 75, 58, 20, 86, 75);
 
 fill("white");
 textSize(40);
-text("happy days", 90,100);
+text("happy days", 300,300);
+fill("gray");
+text("happy days", 300,250);
 }
